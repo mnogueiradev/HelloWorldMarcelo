11faa81b03e992e5341ad0c8f4ba7008b1fb85d2
Lusgago <nogmath185@gmail.com>
2025-05-02 17:10:33 -0300

    Initial commit: Hello World app

---
+++ aula9/app.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from .schemas import Message
+
+app = FastAPI()
+
+@app.get("/", response_model=Message)
+def read_root():
+    return {"Message": "Hello World"}  # Corrigido para "Message" com "M" maiúsculo
